4a4fd9ccd5c52a99e47718b91a6a8fdef2ae98cc
johnmogi <anguru@gmail.com>
2022-08-24 20:32:12 +0300

    up

--- guides/status.md
+++ guides/status.md
@@ -1,5 +1,9 @@
 bulma implimented.
 
-add the form data into an object and from the object fill the output.
+add the form data into an object and from the object fill the output. implimented.
+
+
+add all form fields + date.
 
 add the form data into the localstorage
+
--- js/form.js
+++ js/form.js
@@ -3,38 +3,46 @@ const body = document.querySelector('body')
 const taskCreator = document.getElementById('taskCreator')
 const taskSubmit = document.getElementById('taskSubmit')
 const taskName = document.getElementById('taskName')
+const taskDesc = document.getElementById('taskDesc')
+
 
 const div1 = '<div class="column is-4">'
 const divClose = '</div>'
+const br ='<br/>'
 
-const taskData = {}
-let task_Name = {}
 const tasksArr = []
+const tasksDOM = []
+let valid = false;
 
 function clean(){
-    taskData[task_Name] = ''
 }
 function validate(){
-if (!taskName.value){
-    
-    document.getElementById("output-box").innerHTML = 
-    "name is empty ";
-}
-else{
+    if (!taskName.value){
+        document.getElementById("output-box").innerHTML = "the Task name is empty ";
+        return valid = false;
+    }
+    else if (!taskDesc.value){
+        document.getElementById("output-box").innerHTML = "the Task Description is empty ";
+        return valid = false;
+    }
+    else{
     document.getElementById("task-box").innerHTML += 
-    div1 + taskName.value + divClose
-    task_Name = {'name': taskName.value}
-    tasksArr.push(task_Name)
+    div1 + taskName.value + br +  taskDesc.value + divClose
 
+    return valid = true;
 }
 }
 
 taskSubmit.addEventListener("click", (event) => {
     event.preventDefault();
     validate();
+    if (!valid) {return document.getElementById("output-box").innerHTML += br + "Some of the fields are empty"; }
+    let taskData = {
+        'task name': taskName.value,
+        'task description': taskDesc.value
+    }
+    tasksArr.push(taskData)
     console.log(tasksArr);
+
   }, false);
-
-
-
 
